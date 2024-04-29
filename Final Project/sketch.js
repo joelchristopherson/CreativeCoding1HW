e32afd7317322d21e85f12dbd53d3eedcc1fa53d
Joel W. Christopherson <joel.christopherson@umontana.edu>
2024-04-28 19:25:07 -0600

    Testing

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -4,7 +4,7 @@ function preload() {
   mySound = loadSound('audiofiles/Runoff.mp3');
 }
 
-function setup() {
+/*function setup() {
   let cnv = createCanvas(100, 100);
   cnv.mousePressed(canvasPressed);
   background(220);
@@ -15,4 +15,19 @@ function canvasPressed() {
   // playing a sound file on a user gesture
   // is equivalent to `userStartAudio()`
   mySound.play();
+}*/
+
+function Setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0)
+}
+
+function mouseClicked() {
+  if (mySound.isPlaying()){
+    mySound.pause()
+  } else
+  mySound.play()
 }
