fa9a83e0d55a4b163acc51c54dbcaa2320b695bc
Joel W. Christopherson <joel.christopherson@umontana.edu>
2024-04-28 19:36:21 -0600

    Update sketch.js

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -1,4 +1,5 @@
-let mySound;
+let mySound
+var fft
 function preload() {
   soundFormats('mp3');
   mySound = loadSound('audiofiles/Runoff.mp3');
@@ -19,10 +20,22 @@ function canvasPressed() {
 
 function Setup() {
   createCanvas(windowWidth, windowHeight);
+  fft = new p5.FFT()
 }
 
 function draw() {
   background(0)
+  stroke(255)
+
+  var wave = fft.wavefrom()
+
+  for (var i = 0; i < width; i++) {
+    var index = floor(map(i, 0, width, 0, wave.length))
+
+    var x = i
+    var y = wave[index] * 300 + height /2
+    point(x,y)
+  }
 }
 
 function mouseClicked() {
