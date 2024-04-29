0021c78ad76338364597fed200e9e08c742c919a
Joel W. Christopherson <joel.christopherson@umontana.edu>
2024-04-28 19:41:38 -0600

    Update sketch.js

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -1,41 +1,32 @@
-let mySound
+var mySound
 var fft
+
 function preload() {
-  soundFormats('mp3');
+  //soundFormats('mp3');
   mySound = loadSound('audiofiles/Runoff.mp3');
 }
 
-/*function setup() {
-  let cnv = createCanvas(100, 100);
-  cnv.mousePressed(canvasPressed);
-  background(220);
-  text('tap here to play', 10, 20);
-}
-
-function canvasPressed() {
-  // playing a sound file on a user gesture
-  // is equivalent to `userStartAudio()`
-  mySound.play();
-}*/
-
 function Setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(8000,8000);
   fft = new p5.FFT()
 }
 
 function draw() {
   background(0)
   stroke(255)
+  nofill()
 
   var wave = fft.wavefrom()
 
+  beginShape()
   for (var i = 0; i < width; i++) {
     var index = floor(map(i, 0, width, 0, wave.length))
 
     var x = i
     var y = wave[index] * 300 + height /2
-    point(x,y)
+    vertex(x,y)
   }
+  endShape()
 }
 
 function mouseClicked() {
