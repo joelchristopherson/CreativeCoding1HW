bb9cb9c3538aaeeb6b26a2e0ccc9efcbf9d4c8d3
Joel W. Christopherson <joel.christopherson@umontana.edu>
2024-05-01 21:22:33 -0600

    All done :)

--- Final Project/Readme.md
+++ Final Project/Readme.md
@@ -10,12 +10,42 @@ The purpose of this Readme.md is to describe and document the process of my proj
 4. Next, I started working on my first visualization. I had to do lots of research, but ultimately learned that using a waveform and a Fast Fourier Transform (FFT) would be a great start to getting the visualization to sync with the audio.
 5. Now that I had a basic waveform, I wanted to experiment with how to make it look a little more visually appealing, which included introducing color and mirroring the waveform to make it look more cohesive.
 6. After creating the first waveform, I wanted to work on my second one. I learned how to turn that original waveform into a circle, using some math and a for loop to mirror the first half of the circle. I will touch more on this in the Successes and Failures Section.
-7. Once I had the first two visualization, I started to work on two things. Uploading more than one song and creating drop down menus for both the 
+7. Once I had the first two visualizations, I started to work on two things. Uploading more than one song and creating drop down menus for both the songs and the visualizations. I ended up using a loop to pick through me songs array and a switch case to choose from the three different visualizations.
+8. Once I had the menus figured out I worked on the third visualization which was the spectrum visualization, but quickly turned into the speaker visualization, when I got some feedback from one of my buddies who is also a Management Information Systems major.
+9. After the final visualization was finished, I focused on the more information button where I included information on how to use the application, song credits for the artists, and the name of my application.
+10. To wrap things up I added about 5 more songs and did some rigorous testing. The main things I tested included:
+    - All songs in the program
+    - All songs with all visualizations
+    - Play and pause button on all the songs
+    - Switching songs while the current song was either playing and paused
+    - Accessing the more information menu during the songs
 
 # Successes
+- Dynamic visualization selection: I implemented the ability for users to switch between multiple visualization modes (Circle Visualization, Linear Waveform, Speaker Visualization) and I made it possible to switch during songs.
+- Real-time Audio Analysis: I used Fast Fourier Transform (FFT) for real-time audio analysis, which adjusts each of the visualizations based on the frequency data of the playing song.
+- Particle System for Visualization: I made a particle system where particles react to the music's amplitude, providing a cool visual experience for the users. This was definitely the hardest part of the project.
+- Responsive UI Elements: I added responsive UI elements like dropdowns for song and visualization selection and buttons for playback control, which made the application a lot more user-friendly. (I used css to help with formatting)
+- Help Information: I included a help button that provides users with instructions and song credits for the artists.
 
 # Failures
+- Volume Control Implementation: I was unable to implement the planned volume slider control, which would have allowed users to adjust the playback volume directly from the interface. I figured most users have control of the volume on their device, so I might save some time by leaving this out.
+- Repetitive Debugging: I spent tooooooo much time fixing and refining the same functionalities, which slowed overall project progress. Sometimes I would get a black line in the middle of my circular visualization even though I hadn't changed the code for it.
+- Logic Errors: I had frequent logic errors that required substantial troubleshooting. The biggest example coming from when it came time to introduce multiple visualization as I had worked on the first one in the draw function, instead of its own function.
+- Code Optimization: I struggled with making the code more concise, but this is something that I believe I have improved on since I took app development in the College of Business.
 
 # What I Learned
+- Markdown: I learned a lot more about writing in Markdown.
+- HTML Setup: I learned more about setting up HTML files, including how to structure and link various resources. The newest thing for me is utilizing a cdn instead of local copies of p5.
+- FFT and Waveforms: I learned about Fast Fourier Transforms (FFT) and waveforms, applying these concepts help me to analyze audio signals and create dynamic visualizations for this project.
+- CSS Utilization: I improved my skills in CSS for styling, learning to control the layout and visual aspects of p5.js.
+- IDs vs. Classes: I learned more about the differences between IDs and classes, which helped me organize and style elements in my project.
+- Object-Oriented Programming: I expanded my understanding of objects and methods in JavaScript. Prior to this class I took App Development in the College of Business because I am an MIS major. After that I became a T.A. in App Development when it switch to C#.
+- Passion for Coding: This project reinforced my enjoyment of coding, especially when working on something I am passionate about. I am sad I did not pursue more coding classes while at UM, but am excited to start a career in IT Compliance, who knows maybe one day I will end up as a programmer.
 
 # Sources and Helpful Guides
+- https://p5js.org/reference/#/p5.FFT
+- https://p5js.org/reference/#/p5.FFT/waveform
+- https://p5js.org/reference/#/p5/ellipse
+- https://editor.p5js.org/kiko_QICHEN/sketches/sf_FdCgCX
+- https://www.reddit.com/r/p5js/comments/ynr0sw/is_it_possible_to_make_a_visualizer_in_p5_that/
+- https://p5js.org/reference/#/p5/createSelect
--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -1,144 +1,169 @@
 //Global variables 
 //Holds the song that is selected
 var mySong
-//!!!!!!!
+//Variable for the Fast Fourier Transform (FFT), which allows the waveform to pick up the frequencies from the song.
 var fft
 //Array for the particles in the circle visualization
 var particles = []
 //Holds the visualization that the user selects
-var visualizationSelect; 
+var visualizationSelect;
 //Array for my songs
 var songs = [
   { name: "Till I Leave", file: "audiofiles/tillIleave.mp3" },
   { name: "Whoa (Mind in Awe)", file: "audiofiles/whoa(mindinawe).mp3" },
   { name: "Runoff", file: "audiofiles/Runoff.mp3" },
   { name: "Audio 002", file: "audiofiles/Audio002.mp3" },
-  { name: "Diamondz n Roses", file: "audiofiles/DiamondznRoses.mp3"},
-  { name: "METAMORPHOSIS", file: "audiofiles/METAMORPHOSIS (Sped Up).mp3"}
-  //EDM, Scrillex, Narcos - timmy trumpets, pitbull
+  { name: "Diamondz n Roses", file: "audiofiles/DiamondznRoses.mp3" },
+  { name: "METAMORPHOSIS", file: "audiofiles/METAMORPHOSIS (Sped Up).mp3" },
+  { name: "After Dark", file: "audiofiles/Mr.Kitty - After Dark.mp3" },
+  { name: "Narco", file: "audiofiles/Narco.mp3" },
+  { name: "Your Name", file: "audiofiles/DVRST - YOUR NAME.mp3" },
+  { name: "STARFALL", file: "audiofiles/STARFALL.mp3" }
 ];
 var currentSong;
 var songSelect;
 var playPauseButton;
 
-//Queues up the first song and sets up the program to play mp3 files I upload
+//Queues up the first song
 function preload() {
-  //soundFormats('mp3');
   mySong = loadSound('audiofiles/tillIleave.mp3');
 }
 
 //Adds labels and main interface to the screen
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //Allows the FFT to function correct and look better 
   angleMode(DEGREES)
   fft = new p5.FFT()
 
-   //Creates the label for the dropdown
-   let label = createSpan('Select Song: ');
-   label.id('selectSongLabel');
-   label.position(5, 12.5); 
-  
-   //Creats the drop down menu from the song array
-  songSelect = createSelect(); 
-  songSelect.id('playPauseButton'); 
+  //Creates the label for the dropdown
+  let label = createSpan('Select Song: ');
+  label.id('selectSongLabel');
+  label.position(5, 12.5);
+
+  //Creates the drop down menu from the song array
+  songSelect = createSelect();
+  songSelect.id('playPauseButton');
   songSelect.class('nav-button')
-  songSelect.position(115, 10); 
+  songSelect.position(label.width + 10, 10);
   songs.forEach((song, index) => {
-    songSelect.option(song.name, index); 
+    songSelect.option(song.name, index);
   });
   //Loads the song if it is changed in the menu
   songSelect.changed(loadSelectedSong);
 
-    //Sets up the visualization drop down menu
-    let visLabel = createSpan('Select Visualization: ');
-    visLabel.id('selectSongLabel')
-    visLabel.position(335, 12.5);
-    visualizationSelect = createSelect();
-    visualizationSelect.position(510, 10);
-    visualizationSelect.id('playPauseButton')
-    visualizationSelect.class('nav-button')
-    visualizationSelect.option('Circle Visualization', 'circle');
-    visualizationSelect.option('Linear Waveform', 'linear');
-    visualizationSelect.option('Spectrum Visualization', 'spectrum')
+  //Sets up the visualization drop down menu
+  let visLabel = createSpan('Select Visualization: ');
+  visLabel.id('selectSongLabel')
+  let moveOver = songSelect.position().x + songSelect.size().width + 15;
+  visLabel.position(moveOver, 12.5);
+  visualizationSelect = createSelect();
+  let moveOver2 = visLabel.position().x + visLabel.size().width + 5;
+  visualizationSelect.position(moveOver2, 10);
+  visualizationSelect.id('playPauseButton')
+  visualizationSelect.class('nav-button')
+  visualizationSelect.option('Circle Visualization', 'circle');
+  visualizationSelect.option('Linear Waveform', 'linear');
+  visualizationSelect.option('Speaker Visualization', 'speaker')
 
-    //Sets up the play/pause button 
+  //Sets up the play/pause button 
   playPauseButton = createButton('Play/Pause');
-  playPauseButton.id('playPauseButton'); 
+  playPauseButton.id('playPauseButton');
   playPauseButton.class("nav-button")
   playPauseButton.position(windowWidth - playPauseButton.size().width - 30, 10);
   //Makes the button actually play/pause when the user presses it
-  playPauseButton.mousePressed(togglePlayPause); 
+  playPauseButton.mousePressed(togglePlayPause);
+
+  //Sets up the help/more information button
+  helpButton = createButton('More Information');
+  helpButton.id('playPauseButton');
+  helpButton.class("nav-button");
+  helpButton.position(windowWidth - helpButton.size().width - 30, windowHeight - helpButton.size().height - 20);
+  helpButton.mousePressed(showHelp);
 
   //Stops the song from playing when the program is first loaded, I had to do this because some browsers don't allow for autoplay
   noLoop();
 }
 
+//What the more information button displays
+function showHelp() {
+  alert("Need help? Here's some information on how to use this application.\n 1. Select the song/visualization you'd like to view. \n Note: You can change the visualization while the song plays. \n 2. Press 'Play/Pause' to start and stop the music. \n 3. If all else fails, refresh and wait a few moments after changing the song. \n \n Song Credits: \n g2ox_em, COTIS - till I leave \n XXXTENTACION - whoa (mind in awe) \n Jobii - Runoff \n Next To Blue - Audio 002 \n VaporGod - Diamondz n Roses \n INTERWORLD - METAMORPHOSIS \n Mr. Kitty - After Dark \n Blasterjaxx, Timmy Trumpet - Narco \n DVRST - Your Name \n MoonDeity, ARCHEZ - STARFALL\n \n Thanks for choosing AudioAura! \n \n AudioAura \n Created by Joel Christopherson \n Copyright Spring 2024");
+}
+
 //Checks to see if a song is loading
-let isSongLoading = false; 
+let isSongLoading = false;
 
 //Stops a new song from loading if another is already loading
 function loadSelectedSong() {
-  if (isSongLoading) return; 
+  if (isSongLoading) return;
   isSongLoading = true;
 
   //Selects the correct song from the drop down menu
   let songIndex = songSelect.value();
   let songPath = songs[songIndex].file;
 
+  //Stops the current song if a new song is selected.
   if (mySong.isPlaying()) {
     mySong.stop();
   }
-  
-  //Clears the particles
-  particles = []; 
-  background(0); 
 
-  // Load the new song
-  mySong = loadSound(songPath, function() {
-    isSongLoading = false; // Reset loading state once done
-    fft = new p5.FFT(); // Reinitialize FFT
-    loop(); // Ensure the draw loop is running
+  //Array for the particles in the circular visualization
+  particles = [];
+  background(0);
+
+  //Loads the new song
+  mySong = loadSound(songPath, function () {
+    //Resets the loading state once done
+    isSongLoading = false; 
+    //Setting up the FFT
+    fft = new p5.FFT();
+    //Makes sure the draw loop is running
+    loop(); 
   });
 
-  // Keep the loop running even when a new song is loaded but not yet playing
+  //Keeps the draw loop running when a new song is loaded but has not been played yet 
   if (!mySong.isPlaying() && !looping) {
     loop();
   }
 }
 
+//contains a switch case for the different visualization options.
 function draw() {
   background(0);
   fft.analyze();
   //Grabs the visualization that the user selects from the switchcase below. This was hard for me to figure out once it was time to include more visualizations
   let vizType = visualizationSelect.value();
-  
-  switch(vizType) {
+
+  switch (vizType) {
     case 'circle':
       drawCircleVisualization();
       break;
     case 'linear':
       drawLinearVisualization();
       break;
-    case 'spectrum':
-      drawSpectrumVisualization();
+    case 'speaker':
+      drawSpeakerVisualization();
       break;
   }
 }
 
-//This was the first visualization I worked on
+//This was the first visualization I worked on, and ironically the last due to some formatting as I introduced new features.
 function drawCircleVisualization() {
+  //Places the waveform in the middle of the screen
   translate(width / 2, height / 2);
   strokeWeight(10);
   stroke(255);
-  
+
+  //Combines the waveform with the fft variable
   let wave = fft.waveform();
+  //Loop that creates a half circle and iterates twice to form a full circle, this was intentional so I could fill the circle with color and not have it be black.
   for (let t = -1; t <= 1; t += 2) {
     beginShape();
     for (let i = 0; i <= 180; i += 0.25) {
       let index = floor(map(i, 0, 180, 0, wave.length - 1));
       let radius = map(wave[index], -1, 1, 150, 350);
-      let x = radius * cos(i);
-      let y = radius * sin(i) * t;
+      let x = radius * sin(i) * t;
+      let y = radius * cos(i);
       vertex(x, y);
     }
     endShape();
@@ -146,78 +171,93 @@ function drawCircleVisualization() {
   updateParticles();
 }
 
+//The simpliar version of the circular version for obvious reasons
 function drawLinearVisualization() {
-  let amp = fft.getEnergy(20, 200); // Get amplitude for background and color effects
+  //Gets amplitude for background and color effects
+  let amp = fft.getEnergy(20, 200); 
   let wave = fft.waveform();
 
-  // Set up color mode and stroke properties for a vibrant waveform
+  //Sets up color mode and stroke properties for the waveform to make it colorful.
   colorMode(HSB, 255);
   noFill();
 
-  // Draw the original waveform with a color gradient
+  //Draws the original waveform with a color gradient
   beginShape();
   for (let i = 0; i < width; i++) {
     let index = floor(map(i, 0, width, 0, wave.length));
     let x = i;
-    let col = map(wave[index], -1, 1, 0, 255); // Map amplitude to hue
+    //Ties the waveform with the colors
+    let col = map(wave[index], -1, 1, 0, 255); 
     stroke(col, 255, 255);
     let y = wave[index] * 300 + height / 2;
     vertex(x, y);
   }
   endShape();
 
-  // Draw the mirrored waveform with a color gradient
+  //Draws the waveform in the opposite way with a matching color gradient
   beginShape();
   for (let i = 0; i < width; i++) {
     let index = floor(map(i, 0, width, 0, wave.length));
     let x = i;
-    let col = map(wave[index], -1, 1, 0, 255); // Map amplitude to hue
-    stroke(col, 255, 200);  // Slightly different saturation for visual contrast
+    let col = map(wave[index], -1, 1, 0, 255); 
+    stroke(col, 255, 200); 
     let y = height - (wave[index] * 300 + height / 2);
     vertex(x, y);
   }
   endShape();
-
-  colorMode(RGB, 255); // Reset color mode to default
+//Resets the colormode
+  colorMode(RGB, 255); 
 }
 
-function drawSpectrumVisualization() {
+//Final visualization I worked on
+function drawSpeakerVisualization() {
   let spectrum = fft.analyze();
-  let beat = fft.getEnergy("bass"); // Use bass frequencies for beat detection
-  let numRings = 15;  // Total number of rings
-  let maxRadius = min(width, height) /1.25 ;  // Maximum radius for the outermost ring
+  //Used to analyze the bass from the songs to change the visualization with the beat
+  let beat = fft.getEnergy("bass"); 
+  //Number of rings in the visualization
+  let numRings = 12;  
+  //Sets the max for the outermost circle in the visualization
+  let maxRadius = min(width, height) / 1.25;  
 
-  background(0);  // Black background
-  noFill();  // No fill for the rings
-  strokeWeight(5);  // Width of the rings
+  background(0); 
+  noFill(); 
+  strokeWeight(5);
 
-  push();  // Save the drawing context
-  translate(width / 2, height / 2);  // Move to the center of the canvas
+  //Save the context of the drawing
+  push(); 
+  //Centers on the canvas
+  translate(width / 2, height / 2);
 
+  //Maps each of the rings to a proportion of the fft spectrum
   for (let i = 0; i < numRings; i++) {
-      let freqIndex = floor(map(i, 0, numRings, 0, spectrum.length / 2));  // Map each ring to a portion of the spectrum
-      let amplitude = spectrum[freqIndex];
-      let radius = map(amplitude, 0, 255, 10, maxRadius / numRings * (i + 1));  // Calculate radius based on amplitude
+    let freqIndex = floor(map(i, 0, numRings, 0, spectrum.length / 2)); 
+    let amplitude = spectrum[freqIndex];
+    //Calculates the radius for each of the circles in the fft spectrum
+    let radius = map(amplitude, 0, 255, 10, maxRadius / numRings * (i + 1));  
 
-      // Calculate color based on beat
-      let r = random(50,255);
-      let g = random(50,255);
-      let b = random(50,255);
+    //Calculates a random color based on the beat
+    let r = random(50, 255);
+    let g = random(50, 255);
+    let b = random(50, 255);
 
-      stroke(r, g, b);  // Set color based on beat
-      ellipse(0, 0, radius * 2, radius * 2);  // Draw the ring
+    stroke(r, g, b);
+    //Draws the rings
+    ellipse(0, 0, radius * 2, radius * 2);
   }
-
-  pop();  // Restore the drawing context
+//Restores the circles to the normal sizes
+  pop();  
 }
 
+//Updates the particles on the circular visualization
 function updateParticles() {
   let amp = fft.getEnergy(20, 200);
-  let particleRate = map(amp, 0, 255, 0, 3); // Dynamic particle creation rate based on amplitude
-  // Create particles based on amplitude
+  //Sets the amount of new particles allowed
+  let particleRate = map(amp, 0, 255, 0, 3);
+  //Create particles based on the songs amplitude
   for (let i = 0; i < particleRate; i++) {
     particles.push(new Particle());
   }
+  //updates the particles to reflect the song that is play and detects the bass so the speed can be altered when bass hits in the song.
   for (var i = particles.length - 1; i >= 0; i--) {
     if (mySong.isPlaying()) {
       particles[i].update(amp > 200);
@@ -229,14 +269,15 @@ function updateParticles() {
   }
 }
 
+//Play and Pause button
 function togglePlayPause() {
   if (mySong.isPlaying()) {
     mySong.pause();
     noLoop();
   } else {
     if (!mySong.isLoaded()) {
-      // This will check if the song is loaded before playing
-      mySong = loadSound('audiofiles/tillIleave.mp3', function() {
+      //This will check if the song is loaded before playing
+      mySong = loadSound('audiofiles/tillIleave.mp3', function () {
         mySong.play();
       });
     } else {
@@ -247,24 +288,24 @@ function togglePlayPause() {
 }
 
 
-//Creating a particle object and place around the perimeter of the waveform
-class Particle{
+//Creating a particle object and places around the perimeter of the waveform
+class Particle {
   constructor() {
     this.pos = p5.Vector.random2D().mult(250)
     this.vel = createVector(0, 0)
     this.acc = this.pos.copy().mult(random(0.0001, 0.00001))
 
-    this.w = random(3,12)
+    this.w = random(3, 12)
 
     this.color = [random(100, 255), random(100, 255), random(100, 255)]
   }
 
   //These are methods that allow me to update the particles for the circle visualization
   //This method updates the speed of the particles once the bass is detected
-  update(cond){
+  update(cond) {
     this.vel.add(this.acc)
     this.pos.add(this.vel)
-    if (cond){
+    if (cond) {
       this.pos.add(this.vel)
       this.pos.add(this.vel)
       this.pos.add(this.vel)
@@ -274,13 +315,14 @@ class Particle{
 
   //This method removes the particles from the screen once they get to the edge of the screen
   remove() {
-    if(this.pos.x < -width / 2 || this.pos.x > width / 2||
-    this.pos.y < -height /2 || this.pos.y > width / 2) {
+    if (this.pos.x < -width / 2 || this.pos.x > width / 2 ||
+      this.pos.y < -height / 2 || this.pos.y > width / 2) {
       return true
-    } else{ return false
+    } else {
+      return false
     }
   }
-  
+
   //This method shows the particles and gives them color
   show() {
     noStroke()
